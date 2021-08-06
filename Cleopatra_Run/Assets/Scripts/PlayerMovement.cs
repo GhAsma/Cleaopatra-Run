79f8fd5522439c59f0cd4ef7f04259b92082298b
GhAsma <48518697+GhAsma@users.noreply.github.com>
2021-08-06 14:58:08 +0100

    Add

---
+++ Cleopatra_Run/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed=5;
+    public Rigidbody rb;
+
+
+    private void FixedUpdate()
+    {
+
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(rb.position + forwardMove);
+
+    }
+
+
+}
